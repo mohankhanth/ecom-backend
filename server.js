4391a2ea7a092f19f589e8d2d2b9d6873c65ab37
Mohan Swamy <mohankhanth@gmail.com>
2023-08-28 07:20:44 +0530

    Updated packages.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+// const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
+require('dotenv').config()
+const notFound = require('./middleware/not-found');
+
+app.use(express.json());
+
+// app.use('/api/v1/tasks', tasks);
+
+app.use(notFound);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
