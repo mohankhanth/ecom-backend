acf6afc5a89f04a79fa60ee70c296a2da1374506
Mohan Swamy <mohankhanth@gmail.com>
2023-08-28 07:42:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 const express = require('express')
 const app = express()
+var morgan = require('morgan')
 // const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
 require('dotenv').config()
 const notFound = require('./middleware/not-found');
 
 app.use(express.json());
+app.use(morgan('dev'));
 
 // app.use('/api/v1/tasks', tasks);
+app.use('/', (req,res) => {
+    res.json({"mes": 'Welcome to home page'})
+});
 
 app.use(notFound);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
